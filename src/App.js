99a1a6dbad608c3c4a858085b6104715fc82edf3
Patrick Abbott <patrickmabbott@yahoo.com>
2021-01-06 15:54:35 -0800

    me-01: Initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream
+
+=======
+>>>>>>> Stashed changes
+import React, { Component } from 'react';  
+import './App.css';
+let recipient = "Orlando";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //recipient = recipient + "ly people"
+
+    alert(`Hello ${recipient}`);
+    console.log(`Hello ${recipient}`);
+  }
+
+  render() {
+    return <h1>Hello {recipient}!</h1>;
+  }
+}
+
+export default App;
